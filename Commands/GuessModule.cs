a25b784474f8171a64e09814785d5019319cc425
DrEngi <drengi@users.noreply.github.com>
2019-01-01 21:43:44 -0500

    more stuff

--- Commands/GuessModule.cs
+++ Commands/GuessModule.cs
@@ -8,45 +8,154 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Diagnostics;
 
-public class GuessModule : ModuleBase
+namespace guess_bot
 {
-    private static int Number = -1;
-    private static int Tries = 0;
-    public static bool GameRunning = false;
-    public static ulong ChannelID = 0;
-    
-    [Command("setnumber"), Summary("Set a number for the guess command")]
-    [RequireUserPermission(ChannelPermission.ManageMessages)]
-    public async Task SetNumber([Summary("The number to set the guess to")] int number)
+    public class GuessModule : ModuleBase
     {
-        Number = number;
-        await ReplyAsync("Guess number set to " + Number);
-    }
+        //I know this is bad form but whatever
+        private static int Number = -1;
+        private static int Tries = 0;
+        public static bool GameRunning = false;
+        public static ulong ChannelID = 0;
 
-    [Command("setchannel"), Summary("Set the specified game channel")]
-    [RequireUserPermission(ChannelPermission.ManageMessages)]
-    public async Task SetChannel()
-    {
-        ChannelID = Context.Channel.Id;
-        await ReplyAsync("Channel set.");
-    }
-
-    public static async Task GuessNumber(int number, ISocketMessageChannel channel, IUser user, IGuildChannel guildChannel, CommandContext context)
-    {
-        if (GameRunning)
+        private static string LastWinnerMention = "";
+        private static int LastWinnerCount = 0;
+        private static int LastWinnerTries = 0;
+        private static bool HadLastWinner = false;
+        
+        [Command("lastwinner"), Summary("Gets the last winner")]
+        [RequireUserPermission(ChannelPermission.ManageMessages)]
+        public async Task LastWinner()
         {
-            if (number == Number)
+            if (HadLastWinner)
+            {
+                var emb = new EmbedBuilder();
+                emb.WithTitle("Last Winner");
+                emb.WithCurrentTimestamp();
+                emb.WithColor(Color.Green);
+
+                emb.AddField("Winner", LastWinnerMention);
+                emb.AddField("Tries", LastWinnerTries);
+                emb.AddField("Number", LastWinnerCount);
+                
+                await ReplyAsync("", false, emb.Build());
+            }
+            else
+            {
+                await ReplyAsync("There hasn't been a winner");
+            }
+        }
+
+
+        [Command("setnumber"), Summary("Set a number for the guess command")]
+        [RequireUserPermission(ChannelPermission.ManageMessages)]
+        public async Task SetNumber([Summary("The number to set the guess to")] int number)
+        {
+            Number = number;
+            await ReplyAsync("Guess number set to " + Number);
+        }
+
+        [Command("setchannel"), Summary("Set the specified game channel")]
+        [RequireUserPermission(ChannelPermission.ManageMessages)]
+        public async Task SetChannel(IChannel channel)
+        {
+            ChannelID = channel.Id;
+            await ReplyAsync("Channel set.");
+        }
+
+        public static async Task GuessNumber(int number, ISocketMessageChannel channel, IUser user, IGuildChannel guildChannel, CommandContext context)
+        {
+            if (GameRunning)
+            {
+                if (number == Number)
+                {
+                    GameRunning = false;
+
+                    LastWinnerMention = user.Mention;
+                    LastWinnerCount = Number;
+                    LastWinnerTries = Tries + 1;
+                    HadLastWinner = true;
+                    
+                    Tries++;
+                    RestUserMessage msg = await channel.SendMessageAsync("Congratulations " + user.Mention + ", " + number + " is correct! It took " + Tries + " tries to guess it.");
+                    await msg.PinAsync();
+                    Number = -1;
+                    Tries = 0;
+
+                    var role = context.Guild.GetRole(context.Guild.Roles.FirstOrDefault(x => x.Name.Equals("Member")).Id);
+                    var channelNew = await context.Guild.GetChannelAsync(ChannelID);
+
+                    // If either the of the object does not exist, bail
+                    if (role == null || channelNew == null) return;
+
+                    // Fetches the previous overwrite and bail if one is found
+                    var previousOverwrite = channelNew.GetPermissionOverwrite(role);
+                    if (previousOverwrite.HasValue) await channelNew.RemovePermissionOverwriteAsync(role);
+
+                    // Creates a new OverwritePermissions with send message set to deny and pass it into the method
+                    await channelNew.AddPermissionOverwriteAsync(role,
+                        new OverwritePermissions(sendMessages: PermValue.Deny));
+
+                }
+                else Tries++;
+            }
+        }
+
+        [Command("start"), Summary("Start the game")]
+        [RequireUserPermission(ChannelPermission.ManageMessages)]
+        public async Task StartGame()
+        {
+            if (!GameRunning)
+            {
+                if (ChannelID != 0)
+                {
+                    if (Number != -1)
+                    {
+                        GameRunning = true;
+
+                        var role = Context.Guild.GetRole(Context.Guild.Roles.FirstOrDefault(x => x.Name.Equals("Member")).Id);
+                        var channelNew = await Context.Guild.GetChannelAsync(ChannelID);
+
+                        // If either the of the object does not exist, bail
+                        if (role == null || channelNew == null) return;
+
+                        // Fetches the previous overwrite and bail if one is found
+                        var previousOverwrite = channelNew.GetPermissionOverwrite(role);
+                        if (previousOverwrite.HasValue) await channelNew.RemovePermissionOverwriteAsync(role);
+
+                        // Creates a new OverwritePermissions with send message set to deny and pass it into the method
+                        await channelNew.AddPermissionOverwriteAsync(role,
+                            new OverwritePermissions(sendMessages: PermValue.Allow));
+                        
+                        await ReplyAsync("A game has started!");
+                        await (await Context.Guild.GetTextChannelAsync(ChannelID)).SendMessageAsync("A game has started. Guess the number! No decimals");
+                    }
+                    else
+                    {
+                        await ReplyAsync("A number has not been set yet. Set it with `g. setnumber <number>`");
+                    }
+                }
+                else
+                {
+                    await ReplyAsync("A channel has not been set. Set the channel with `g. setchannel` in the correct channel.");
+                }
+            }
+            else
+            {
+                await ReplyAsync("A game is already running");
+            }
+        }
+
+        [Command("stop"), Summary("Stops the game")]
+        [RequireUserPermission(ChannelPermission.ManageMessages)]
+        public async Task StopGame()
+        {
+            if (GameRunning)
             {
                 GameRunning = false;
-                
-                Tries++;
-                RestUserMessage msg = await channel.SendMessageAsync("Congratulations " + user.Mention + ", " + number + " is correct! It took " + Tries + " tries to guess it.");
-                await msg.PinAsync();
-                Number = -1;
-                Tries = 0;
 
-                var role = context.Guild.GetRole(context.Guild.Roles.FirstOrDefault(x => x.Name.Equals("Member")).Id);
-                var channelNew = await context.Guild.GetChannelAsync(ChannelID);
+                var role = Context.Guild.GetRole(Context.Guild.Roles.FirstOrDefault(x => x.Name.Equals("Member")).Id);
+                var channelNew = await Context.Guild.GetChannelAsync(ChannelID);
 
                 // If either the of the object does not exist, bail
                 if (role == null || channelNew == null) return;
@@ -59,83 +168,12 @@ public class GuessModule : ModuleBase
                 await channelNew.AddPermissionOverwriteAsync(role,
                     new OverwritePermissions(sendMessages: PermValue.Deny));
 
-            }
-            else Tries++;
-        }
-    }
-
-    [Command("start"), Summary("Start the game")]
-    [RequireUserPermission(ChannelPermission.ManageMessages)]
-    public async Task StartGame()
-    {
-        if (!GameRunning)
-        {
-            if (ChannelID != 0)
-            {
-                if (Number != -1)
-                {
-                    GameRunning = true;
-
-                    var role = Context.Guild.GetRole(Context.Guild.Roles.FirstOrDefault(x => x.Name.Equals("Member")).Id);
-                    var channelNew = await Context.Guild.GetChannelAsync(ChannelID);
-
-                    // If either the of the object does not exist, bail
-                    if (role == null || channelNew == null) return;
-
-                    // Fetches the previous overwrite and bail if one is found
-                    var previousOverwrite = channelNew.GetPermissionOverwrite(role);
-                    if (previousOverwrite.HasValue) await channelNew.RemovePermissionOverwriteAsync(role);
-
-                    // Creates a new OverwritePermissions with send message set to deny and pass it into the method
-                    await channelNew.AddPermissionOverwriteAsync(role,
-                        new OverwritePermissions(sendMessages: PermValue.Allow));
-                    
-                    await ReplyAsync("A game has started!");
-                    await (await Context.Guild.GetTextChannelAsync(ChannelID)).SendMessageAsync("A game has started. Guess the number! No decimals");
-                }
-                else
-                {
-                    await ReplyAsync("A number has not been set yet. Set it with `g. setnumber <number>`");
-                }
+                await ReplyAsync("A game has been stoppoed");
             }
             else
             {
-                await ReplyAsync("A channel has not been set. Set the channel with `g. setchannel` in the correct channel.");
+                await ReplyAsync("A game is not running");
             }
-        }
-        else
-        {
-            await ReplyAsync("A game is already running");
-        }
-    }
-
-    [Command("stop"), Summary("Stops the game")]
-    [RequireUserPermission(ChannelPermission.ManageMessages)]
-    public async Task StopGame()
-    {
-        if (GameRunning)
-        {
-            GameRunning = false;
-
-            var role = Context.Guild.GetRole(Context.Guild.Roles.FirstOrDefault(x => x.Name.Equals("Member")).Id);
-            var channelNew = await Context.Guild.GetChannelAsync(ChannelID);
-
-            // If either the of the object does not exist, bail
-            if (role == null || channelNew == null) return;
-
-            // Fetches the previous overwrite and bail if one is found
-            var previousOverwrite = channelNew.GetPermissionOverwrite(role);
-            if (previousOverwrite.HasValue) await channelNew.RemovePermissionOverwriteAsync(role);
-
-            // Creates a new OverwritePermissions with send message set to deny and pass it into the method
-            await channelNew.AddPermissionOverwriteAsync(role,
-                new OverwritePermissions(sendMessages: PermValue.Deny));
-
-            await ReplyAsync("A game has been stoppoed");
-        }
-        else
-        {
-            await ReplyAsync("A game is not running");
         }
     }
 }
